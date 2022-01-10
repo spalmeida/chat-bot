c3d8e71fc99cc689c51959f16af6f46677d379d0
samuel do prado almeida <samuelprado.a@gmail.com>
2022-01-10 12:08:08 -0300

    Update chat.php

--- chat.php
+++ chat.php
@@ -13,20 +13,6 @@
 require_once 'db/db.php';
 date_default_timezone_set('America/Sao_Paulo');
 
-function loadScripts() {
-
-    $version = '2.2';
-
-    /*JS*/
-    wp_enqueue_script( 'js-jquery', plugins_url( 'dist/js/jquery.min.js', __FILE__ ), array(), $version );
-    wp_enqueue_script( 'js-scrollbar', plugins_url( 'dist/js/scrollbar.concat.min.js', __FILE__ ), array(), $version );
-    wp_enqueue_script( 'js-script', plugins_url( 'dist/js/script.js', __FILE__ ), array(), $version );
-
-    /*CSS*/
-    wp_enqueue_style( 'css-style',plugin_dir_url(__FILE__) . 'dist/css/style.css', get_stylesheet_uri() , $version);
-    wp_enqueue_style( 'css-scrollbar',plugin_dir_url(__FILE__) . 'dist/css/scrollbar.min.css', get_stylesheet_uri() , $version);
-}
-
 function wpdocs_register_my_custom_menu_page() {
     add_menu_page(
         __( 'Chat BOT', 'textdomain' ),
@@ -39,25 +25,26 @@ function wpdocs_register_my_custom_menu_page() {
     );
 }
 add_action( 'admin_menu', 'wpdocs_register_my_custom_menu_page' );
+
+
+
+
+function loadScripts() {
+    $version = '2.4';
+
+    /*JS*/
+    wp_enqueue_script( 'js-jquery', plugins_url( 'dist/js/jquery.min.js', __FILE__ ), array(), $version );
+    wp_enqueue_script( 'js-scrollbar', plugins_url( 'dist/js/scrollbar.concat.min.js', __FILE__ ), array(), $version );
+    wp_enqueue_script( 'js-script', plugins_url( 'dist/js/script.js', __FILE__ ), array(), $version );
+
+    /*CSS*/
+    wp_enqueue_style( 'css-style',plugin_dir_url(__FILE__) . 'dist/css/style.css', get_stylesheet_uri() , $version);
+    wp_enqueue_style( 'css-scrollbar',plugin_dir_url(__FILE__) . 'dist/css/scrollbar.min.css', get_stylesheet_uri() , $version);
+}
 add_action('wp_enqueue_scripts', 'loadScripts');
 
 
-function chat() { 
-    get_template_part( 'content', 'notitle' );
-    return require_once 'chat_template.php'; 
+function chat_template() { 
+ require_once 'chat_template.php'; 
 } 
-add_shortcode('chat', 'chat'); 
-
-
-function your_wp_title( $title, $sep ) {
-    global $paged, $page;
-    if(is_front_page()){        
-        $title = get_bloginfo( 'name' );   
-        return $title;
-    } else {
-        if ( is_feed() )            
-        // Add the site name.
-            return $title;
-    }
-}
-add_filter( 'wp_title', 'your_wp_title', 10, 2 );
+add_shortcode('chat', 'chat_template'); 
