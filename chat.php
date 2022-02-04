29d0a89c77a1f545a455cd12303314fabe785fd5
Samuel <salmeida@mcontigo.com>
2022-02-04 23:52:49 +0000

    refactor: update plugin

--- chat.php
+++ chat.php
@@ -1,35 +1,32 @@
 <?php
-//teste
+
 /**
  * Plugin Name: Chat BOT
  * Plugin URI: https://agencia.life
  * Description: Chat BOT - Agência Life.
- * Version: 1.1.5
+ * Version: 1.1.3
  * Author: Agência Life
  * Author URI: https://agencia.life
  * License: GPL2
  */
 
-//require_once 'plugin-update/plugin-update-checker.php';
-require_once 'db/db.php';
 require_once 'plugin-update/plugin-update-checker.php';
+require_once 'db/db.php';
 
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'chat-bot', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/worldvisual/chat-bot', // the GitHub API url of your GitHub repo
-        'raw_url' => 'https://raw.github.com/worldvisual/chat-bot/master', // the GitHub raw url of your GitHub repo
-        'github_url' => 'https://github.com/worldvisual/chat-bot', // the GitHub url of your GitHub repo
-        'zip_url' => 'https://github.com/worldvisual/chat-bot/zipball/master', // the zip url of the GitHub repo
-        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '3.0', // which version of WordPress does your plugin require?
-        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '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', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
-    );
-    new WP_GitHub_Updater($config);
-}
+$config = array(
+    'slug' => plugin_basename(__FILE__),
+    'proper_folder_name' => 'chat-bot',
+    'api_url' => 'https://api.github.com/repos/worldvisual/chat-bot',
+    'raw_url' => 'https://raw.github.com/worldvisual/chat-bot/master',
+    'github_url' => 'https://github.com/worldvisual/chat-bot',
+    'zip_url' => 'https://github.com/worldvisual/chat-bot/zipball/master',
+    'sslverify' => true,
+    'requires' => '1.0',
+    'tested' => '3.3',
+    'readme' => 'README.md',
+    'access_token' => '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',
+);
+new WP_GitHub_Updater($config);
 
 
 /*
